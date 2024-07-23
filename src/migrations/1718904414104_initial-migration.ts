235ae6fd6cd07a4f74bafc3dcff35bb003db4a32
Braian Mellor <braianj@gmail.com>
2024-07-23 12:10:02 -0300

    feat: change migration adding mission

--- src/migrations/1718904414104_initial-migration.ts
+++ src/migrations/1718904414104_initial-migration.ts
@@ -35,6 +35,10 @@ export async function up(pgm: MigrationBuilder): Promise<void> {
       type: PgType.VARCHAR,
       notNull: true
     },
+    user_name: {
+      type: PgType.VARCHAR,
+      notNull: true
+    },
     level: {
       type: PgType.INT,
       notNull: false
@@ -64,22 +68,13 @@ export async function up(pgm: MigrationBuilder): Promise<void> {
   pgm.createIndex('progress', ['game_id', 'user_address'])
   pgm.createIndex('progress', ['game_id', 'user_address', 'updated_at'], { unique: true })
 
-  pgm.createTable('challenges', {
+  pgm.createTable('missions', {
     id: {
       type: PgType.UUID,
       primaryKey: true
     },
     description: {
       type: PgType.TEXT,
-      notNull: true
-    },
-    game_id: {
-      type: PgType.UUID,
-      notNull: true,
-      references: 'games'
-    },
-    target_level: {
-      type: PgType.INTEGER,
       notNull: true
     },
     campaign_key: {
@@ -94,32 +89,100 @@ export async function up(pgm: MigrationBuilder): Promise<void> {
     }
   })
 
-  pgm.createIndex('challenges', ['game_id'])
-
-  pgm.createTable(
-    'user_challenges',
-    {
-      user_address: {
-        type: PgType.VARCHAR,
-        notNull: true
-      },
-      challenge_id: {
-        type: PgType.UUID,
-        notNull: true,
-        references: 'challenges'
-      }
+  pgm.createTable('user_missions', {
+    id: {
+      type: PgType.UUID,
+      primaryKey: true
     },
-    {
-      constraints: {
-        primaryKey: ['user_address', 'challenge_id']
-      }
+    user_address: {
+      type: PgType.VARCHAR,
+      notNull: true
+    },
+    mission_id: {
+      type: PgType.UUID,
+      notNull: true,
+      references: 'missions'
+    },
+    start_time: {
+      type: PgType.TIMESTAMP,
+      default: pgm.func('now()')
+    },
+    end_time: {
+      type: PgType.TIMESTAMP,
+      notNull: false
+    },
+    active: {
+      type: PgType.BOOL,
+      notNull: true,
+      default: true
     }
-  )
+  })
+
+  pgm.createIndex('user_missions', ['user_address', 'mission_id', 'active'], { unique: true })
+
+  pgm.createTable('challenges', {
+    id: {
+      type: PgType.UUID,
+      primaryKey: true
+    },
+    description: {
+      type: PgType.TEXT,
+      notNull: true
+    },
+    game_id: {
+      type: PgType.UUID,
+      notNull: true,
+      references: 'games'
+    },
+    mission_id: {
+      type: PgType.UUID,
+      notNull: true,
+      references: 'missions'
+    },
+    target_level: {
+      type: PgType.INTEGER,
+      notNull: true
+    },
+    data: {
+      type: PgType.JSON,
+      notNull: false
+    },
+    active: {
+      type: PgType.BOOL,
+      notNull: true,
+      default: true
+    }
+  })
+
+  pgm.createIndex('challenges', ['game_id', 'mission_id'])
+
+  pgm.createTable('user_challenges', {
+    id: {
+      type: PgType.UUID,
+      primaryKey: true
+    },
+    user_address: {
+      type: PgType.VARCHAR,
+      notNull: true
+    },
+    challenge_id: {
+      type: PgType.UUID,
+      notNull: true,
+      references: 'challenges'
+    },
+    challenge_uncompleted: {
+      type: PgType.BOOL,
+      notNull: true,
+      default: false
+    }
+  })
 }
 
 export async function down(pgm: MigrationBuilder): Promise<void> {
   pgm.dropTable('user_challenges')
   pgm.dropTable('challenges')
+  pgm.dropTable('user_missions')
+  pgm.dropTable('missions')
   pgm.dropTable('progress')
   pgm.dropTable('games')
 }
