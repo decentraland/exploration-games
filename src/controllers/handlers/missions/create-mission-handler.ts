6b6f5686e44d22ee9aad64f786da7a7806d07504
Braian Mellor <braianj@gmail.com>
2024-07-23 22:21:34 -0300

    feat: create description handler and test

---
+++ src/controllers/handlers/missions/create-mission-handler.ts
@@ -0,0 +1,39 @@
+import Joi from 'joi'
+import { InvalidRequestError } from '@dcl/platform-server-commons/dist/errors'
+import { parseJson } from '@dcl/platform-server-commons/dist/utils'
+import { HandlerContextWithPath, Mission } from '../../../types'
+
+const schema = Joi.object<Mission>().keys({
+  description: Joi.string().required(),
+  campaignKey: Joi.string().required()
+})
+
+export async function createMissionHandler(
+  ctx: Pick<HandlerContextWithPath<'db' | 'logs', '/missions'>, 'components' | 'request'>
+) {
+  const {
+    components: { logs, db },
+    request
+  } = ctx
+
+  const logger = logs.getLogger('create-mission-handler')
+
+  const body = await parseJson(request)
+
+  const validate = schema.validate(body)
+  if (validate.error) {
+    logger.warn(`Invalid mission object received when creating: ${validate.error.message} (${JSON.stringify(body)}`)
+    throw new InvalidRequestError(validate.error.message)
+  }
+
+  const validatedBody = body as Mission
+
+  const mission = await db.createMission(validatedBody.description, validatedBody.campaignKey)
+
+  return {
+    status: 201,
+    body: {
+      data: mission
+    }
+  }
+}
---
+++ test/integration/missions/create-mission-handler.spec.ts
@@ -0,0 +1,69 @@
+import { test } from '../../components'
+import { VALID_CAMPAIGN_KEY } from '../../mocks/send-reward-mock'
+import { getIdentity, makeRequest } from '../../utils'
+
+test('POST /api/missions', ({ components }) => {
+  let payload
+  beforeAll(async () => {
+    payload = {
+      description: 'Mission Test',
+      campaignKey: VALID_CAMPAIGN_KEY
+    }
+  })
+
+  it('should return 201 created', async () => {
+    const { localFetch } = components
+
+    const response = await makeRequest(localFetch, '/api/missions', {
+      method: 'POST',
+      body: JSON.stringify(payload)
+    })
+
+    expect(response.status).toBe(201)
+
+    const body = await response.json()
+    console.log(body)
+
+    expect(body.data).not.toBe(undefined)
+    expect(body.data.description).toBe(payload.description)
+    expect(body.data.campaign_key).toBe(payload.campaignKey)
+  })
+
+  it('should return 400 when no auth', async () => {
+    const { localFetch } = components
+
+    const response = await localFetch.fetch('/api/missions', {
+      method: 'POST',
+      body: JSON.stringify(payload)
+    })
+
+    expect(response.status).toBe(400)
+  })
+
+  it('should return 400 when bad payload', async () => {
+    const { localFetch } = components
+
+    const response = await makeRequest(localFetch, '/api/missions', {
+      method: 'POST',
+      body: JSON.stringify({ ...payload, description: 123 })
+    })
+    expect(response.status).toBe(400)
+  })
+
+  it('should return 403 when is not an admin', async () => {
+    const { localFetch } = components
+
+    const newIdentity = await getIdentity()
+
+    const response = await makeRequest(
+      localFetch,
+      '/api/missions',
+      {
+        method: 'POST',
+        body: JSON.stringify(payload)
+      },
+      newIdentity
+    )
+    expect(response.status).toBe(403)
+  })
+})
