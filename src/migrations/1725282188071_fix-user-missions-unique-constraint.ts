3d83c7529969814ad0cb2ce127ea2aa687337cea
Kevin Szuchet <31735779+kevinszuchet@users.noreply.github.com>
2024-09-02 20:55:54 +0100

    Update src/migrations/1725282188071_fix-user-missions-unique-constraint.ts
    Co-authored-by: Braian Mellor <braianj@gmail.com>
    Signed-off-by: Kevin Szuchet <31735779+kevinszuchet@users.noreply.github.com>

--- src/migrations/1725282188071_fix-user-missions-unique-constraint.ts
+++ src/migrations/1725282188071_fix-user-missions-unique-constraint.ts
@@ -17,6 +17,6 @@ export async function up(pgm: MigrationBuilder): Promise<void> {
 }
 
 export async function down(pgm: MigrationBuilder): Promise<void> {
-  pgm.createIndex(tableName, oldColumns, oldOptions)
   pgm.dropIndex(tableName, newColumns, newOptions)
+  pgm.createIndex(tableName, oldColumns, oldOptions)
 }
