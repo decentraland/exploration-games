d7c66b12d3dc9a70b2b52a120d9a7b281d025d5d
Pejosonic <bobrow@gmail.com>
2025-01-23 11:55:59 -0300

    feat: mission start check time after uuid

--- src/controllers/handlers/missions/create-user-mission-handler.ts
+++ src/controllers/handlers/missions/create-user-mission-handler.ts
@@ -18,18 +18,6 @@ export async function createUserMissionHandler(
   const { id: missionId } = params
   const logger = logs.getLogger('create-mission-handler')
 
-  const completedData = await getMissionsByStatus(MissionStatus.COMPLETED, verification!.auth, db)
-  const lastMission = completedData.missions.sort((m1, m2) => parseInt(m2.start_time) - parseInt(m1.start_time))[0]
-
-  const missionTimeWindow = await config.requireNumber('MISSION_TIME_WINDOW_HS')
-  const lastStartTime = lastMission?.start_time || '0'
-  const isTimeForNewMission = Date.now() - missionTimeWindow * 60 * 60 * 1000 > parseInt(lastStartTime)
-
-  if (!isTimeForNewMission) {
-    logger.warn('Need to wait more time for starting a new mission')
-    throw new InvalidRequestError('Need to wait more time for starting a new mission')
-  }
-
   const validateMissionId = uuidSchema.validate(missionId)
 
   if (validateMissionId.error) {
@@ -42,6 +30,18 @@ export async function createUserMissionHandler(
   if (!mission) {
     logger.warn(`No mission found with UUID: ${missionId}`)
     throw new InvalidRequestError('No mission found with this UUID')
+  }
+
+  const completedData = await getMissionsByStatus(MissionStatus.COMPLETED, verification!.auth, db)
+  const lastMission = completedData.missions.sort((m1, m2) => parseInt(m2.start_time) - parseInt(m1.start_time))[0]
+
+  const missionTimeWindow = await config.requireNumber('MISSION_TIME_WINDOW_HS')
+  const lastStartTime = lastMission?.start_time || '0'
+  const isTimeForNewMission = Date.now() - missionTimeWindow * 60 * 60 * 1000 > parseInt(lastStartTime)
+
+  if (!isTimeForNewMission) {
+    logger.warn('Need to wait more time for starting a new mission')
+    throw new InvalidRequestError('Need to wait more time for starting a new mission')
   }
 
   await db.setMissionAsStart(verification!.auth, missionId)
